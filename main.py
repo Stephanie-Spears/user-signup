d669b8ac8d4cb5d88c9d8897ef2bf2b05b6c85d5
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-07-08 22:05:47 -0700

    refactor main.py to handle all requests, removing separate function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template
 import cgi
 import re
 
@@ -6,33 +6,32 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/user-signup", methods=['POST'])
-def get_signup():
-    username = request.form['username']
-    password = request.form['password']
-    verify = request.form['verify']
-    email = request.form['email']
 
-    if not username or not re.match("^[a-zA-Z0-9_]{3,20}$", username):
-        error = "Username error"
-        return redirect("/?error=" + error)
-    if not password or not re.match("^[a-zA-Z0-9_]{3,20}$", password):
-        error = "Password error"
-        return redirect("/?error=" + error)
-    if not verify or (password != verify):
-        error = "Verify error"
-        return redirect("/?error=" + error)
-    if email:
-        if not re.match("^[a-zA-Z0-9_+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$", email) or re.match("^[a-zA-Z0-9_]{3,20}$", email):
-            error = "Email error"
-            return redirect("/?error=" + error)
-
-    return render_template('welcome.html', username=username)
-
-
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        email = request.form['email']
+        if not username or not re.match("^[a-zA-Z0-9_]{3,20}$", username):
+            error = "Username error"
+            return render_template("index.html", error=error)
+        if not password or not re.match("^[a-zA-Z0-9_]{3,20}$", password):
+            error = "Password error"
+            return render_template("index.html", error=error)
+        if not verify or (password != verify):
+            error = "Verify error"
+            return render_template("index.html", error=error)
+        if email:
+            if not re.match("^[a-zA-Z0-9_+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$", email) or re.match("^[a-zA-Z0-9_]{3,20}$", email):
+                error = "Email error"
+                return render_template("index.html", error=error)
+        return render_template('welcome.html', username=username)
+
     encoded_error = request.args.get("error")
     return render_template('index.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
